a30b10da9ad2d5ab16580a2e369eb31345fb393e
vladimirvolkovv <vladimirvolkov93@gmail.com>
2021-06-23 16:51:14 +0300

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Добро пожаловать в greenshop!</h1>
+      <Footer />
     </>
   );
 }
