4620ee27be1830fac4bc125a4c29248caff30264
vladimirvolkovv <vladimirvolkov93@gmail.com>
2021-06-23 20:06:42 +0300

    making header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Добро пожаловать в greenshop!</h1>
+      <main>
+        <Container>
+          <h1>Добро пожаловать в greenshop!</h1>
+        </Container>
+      </main>
       <Footer />
     </>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect sticky='top'>
+        <Navbar.Brand href='/'>GreenShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='/cart'>Корзина</Nav.Link>
+            <Nav.Link href='/login'>Вход</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
