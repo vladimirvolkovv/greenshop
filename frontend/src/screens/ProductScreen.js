d41564a3ff6faaaaa66df4ad2ac37688c1424142
vladimirvolkovv <vladimirvolkov93@gmail.com>
2021-06-25 21:01:12 +0300

    Добавлена страница продукта

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
 
-const ProductScreen = () => {
-  return <div></div>;
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+
+  return (
+    <>
+      <Link className='btn btn-primary my-3' to='/'>
+        Назад
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} отзывов`} />
+            </ListGroup.Item>
+            <ListGroup.Item>Цена: {product.price} руб.</ListGroup.Item>
+            <ListGroup.Item>Описание: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Цена:</Col>
+                  <Col>
+                    <strong>{product.price} руб.</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Статус:</Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'В наличии' : 'Закончилось'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                  Добавить в корзину
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
