32f58d03b06fca202432808b2e8fc60628c45294
vladimirvolkovv <vladimirvolkov93@gmail.com>
2021-06-29 15:57:23 +0300

    добавлена регистрация пользователя для backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,6 +22,33 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error('Такой пользователь уже существует');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   if (user) {
@@ -32,4 +59,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
