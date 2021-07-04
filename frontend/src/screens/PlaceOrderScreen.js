9916df58cb8c9b45e3c3ce09665914cebfaf5e8a
vladimirvolkovv <vladimirvolkov93@gmail.com>
2021-07-04 22:12:48 +0300

    order screen added

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,75 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import CheckoutSteps from '../components/CheckoutSteps';
 
 const PlaceOrderScreen = () => {
-  return <div></div>;
+  const cart = useSelector((state) => state.cart);
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Доставка</h2>
+              <p>
+                <strong>Адрес: </strong>
+                {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}, {cart.shippingAddress.city},
+                {cart.shippingAddress.address}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Способ оплаты: </h2>
+              <strong>{cart.paymentMethod}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Список товаров</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Ваша корзина пуста</Message>
+              ) : (
+                <ListGroup variant='flush'>
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={item._id}>
+                      <Row>
+                        <Col md={1}>
+                          <Image src={item.image} alt={item.name} fluid rounded></Image>
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item.product}`}>{item.name}</Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x {item.price} руб. = {item.qty * item.price} руб.
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h2>Сумма заказа: </h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Товары</Col>
+                  <Col>{cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default PlaceOrderScreen;
